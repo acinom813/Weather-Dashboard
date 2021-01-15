1e6bed6f9c54ee3a73517cd62945d7680435c7a5
Monica Hill <monica.r.hill@gmail.com>
2021-01-14 20:52:07 -0500

    update html, css, begin js

---
+++ script.js
@@ -0,0 +1,6 @@
+//1.City name is entered, search is conducted with API
+//2.API returns current weather conditions for the city to include: city name, date,
+//temperature, humidity, wind speed, and UV index.
+//3. 5-day forecast is displayed for the searched city and includes: date, weather icon,
+// humidity, and temperature.
+//4.City name is stored to localStorage for user to retrieve at later time.
